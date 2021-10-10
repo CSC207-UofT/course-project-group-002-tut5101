6504b060985359d05e0ddb0782730f348d939448
ddx <xiededong001211@gmail.com>
2021-10-10 14:56:16 -0400

    Update OrderList.java

---
+++ src/main/OrderList.java
@@ -0,0 +1,11 @@
+/*
+This is the class of Orderlist, it is used to keep track of all orders of the restaurant
+ */
+public class OrderList {
+
+    private List<Order> orderList;
+
+    public void OrderList() {
+        orderList = new ArrayList<Order>();
+    }
+}
