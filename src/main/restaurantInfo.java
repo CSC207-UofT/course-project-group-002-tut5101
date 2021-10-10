f35d5ebceadd302aefcaa408883bad8e1cc05f6e
Raymond <raymondk.liu@mail.utoronto.ca>
2021-10-10 15:29:02 -0400

    Added restaurantInfo class.

---
+++ src/main/restaurantInfo.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.ArrayList;
+
+public class restaurantInfo {
+    public final static int DELIVERY_RANGE = 5000;
+    public final static String ADDRESS = "fake address xxoo";
+    public final static String NAME = "Restaurant X";
+    public static List<KitchenStaff> KitchenStaffs = new ArrayList<>();
+    public static List<ServingStaff> ServingStaffs = new ArrayList<>();
+}
