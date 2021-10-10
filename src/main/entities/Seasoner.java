c3127d49ace75cd0c2b3f04d784067feaa366438
george <shaojie.dong@mail.utronto.ca>
2021-10-10 17:36:47 -0400

    testing_george

---
+++ src/main/entities/Seasoner.java
@@ -0,0 +1,9 @@
+public class Seasoner extends Material{
+    public Seasoner(name:String, usedup: boolean,price: double,quantity: double, freshness: String
+                                       ImportDate: int){
+        super(name, usedup, price);
+        this.freshness = freshness;
+        this.ImportDate = ImportDate;}
+
+
+    }
