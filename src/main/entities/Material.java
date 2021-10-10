068690d9bd3d74c15c4f989fb5384e1a62ee424a
george <shaojie.dong@mail.utronto.ca>
2021-10-10 17:34:31 -0400

    testing_george

--- src/main/entities/Material.java
+++ src/main/entities/Material.java
@@ -1,2 +1,19 @@
-public class Material {
+public abstract class Material extends Inventory implements Expiryable{
+    public String freshness;
+    public int ImportDate
+
+    public Product(name: String, usedup: boolean,price: double,quantity: double, freshness: String
+                    ImportDate: int){
+        super(name, usedup, price, quantity);
+        this.freshness = freshness;
+        this.ImportDate = ImportDate;
+        }
+
+    public String getfreshness(){
+        return this.freshness;
+        }
+
+    public void setFreshness(String NewFreshness){
+        this.freshness = NewFreshness;
+    }
 }
