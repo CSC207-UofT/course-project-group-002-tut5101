ccecdd2a9ca41a0343c718f55bb79abe03c50209
MeeskaM00ska <nathannnnn.li@mail.utoronto.ca>
2021-10-10 16:33:56 -0400

    test for the 3rd time

--- src/main/entities/Review.java
+++ src/main/entities/Review.java
@@ -1,7 +1,22 @@
 package entities;
 
+import java.util.Objects;
+
 public class Review{
     public String user_name;
-    public boolean anonymentYESorNo;
+    public String if_anonymous;
+
+    public Review(String name, String YorN){
+        this.user_name = name;
+        this.if_anonymous = YorN;
+    }
+
+    public String is_anonymous(){
+        if(Objects.equals(this.if_anonymous, "YES")){
+            return "anonymous";
+        } else{
+            return this.user_name;
+        }
+    }
 
 }
