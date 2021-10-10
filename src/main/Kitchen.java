597283a0ea4af29e87f8d61119ab76f78b3f9df3
ddx <xiededong001211@gmail.com>
2021-10-10 18:08:39 -0400

    Modified currentOrder.updateDishStatus in Kitchen class.

--- src/main/Kitchen.java
+++ src/main/Kitchen.java
@@ -25,7 +25,7 @@ public class Kitchen {
 
     private static void updateDishStatus(Dish dish){
         // TODO: throw exceptions if currentOrder is null?
-        currentOrder.updateDishStatus("Order Cooked", dish);
+        currentOrder.updateDishStatus(dish);
 
         if (currentOrder.getOrderDineInOrTakeOut().equals("Dine In")){
             servingBuffer.add(dish);
