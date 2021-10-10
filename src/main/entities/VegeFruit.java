cea0bf674a0563b5ebd81d3c1f476103fa67d641
george <shaojie.dong@mail.utronto.ca>
2021-10-10 17:38:52 -0400

    testing_george

---
+++ src/main/entities/VegeFruit.java
@@ -0,0 +1,9 @@
+public class VegeFruit extends Material{
+    public VegeFruit(name:String, usedup: boolean,price: double,quantity: double, freshness: String
+                                       ImportDate: int){
+        super(name, usedup, price);
+        this.freshness = freshness;
+        this.ImportDate = ImportDate;}
+
+
+    }
