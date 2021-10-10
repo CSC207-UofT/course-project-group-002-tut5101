e9bafb7c33482906fb7e61ec2977ab643b7431f4
ddx <xiededong001211@gmail.com>
2021-10-10 15:21:52 -0400

    Update OrderList.java, add Order.java, OrderListTest.java

--- src/main/OrderList.java
+++ src/main/OrderList.java
@@ -1,4 +1,8 @@
 package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 This is the class of Orderlist, it is used to keep track of all orders of the restaurant
  */
@@ -8,6 +12,30 @@ public class OrderList {
 
     public void OrderList() {
         orderList = new ArrayList<Order>();
+    }
+
+    /**
+     * Try adding order to the order list, if order location too far, reject the order.
+     * @param newOrder The order to be added to the orderList
+     * @return True on successful add, false otherwise
+     */
+    public boolean addOrder(Order newOrder) {
+        /*
+        if (newOrder.getdistance() > RestaurantInfo.DELIVERY_RANGE) {
+            return false;
+        }
+        */
+        orderList.add(newOrder);
+        return true;
+    }
+
+    /**
+     * Get the next order in the orderList to be cooked, if no order found, return null
+     * @return The next order in orderList to be cooked, if no next order, return null
+     */
+    public Order getNextOrderToBeCooked() {
+        Order nextToBeCooked = null;
+
     }
 
     public static void main(String[] args) {
