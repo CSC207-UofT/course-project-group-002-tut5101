bffa7cadc734110df01990c500db9c38018f78e6
george <shaojie.dong@mail.utronto.ca>
2021-10-10 17:36:41 -0400

    testing_george

---
+++ src/main/entities/Seafood.java
@@ -0,0 +1,10 @@
+public class Seafood extends Material{
+    public Seafood(name:String, usedup: boolean,price: double,quantity: double, freshness: String
+                                       ImportDate: int){
+        super(name, usedup, price);
+        this.freshness = freshness;
+        this.ImportDate = ImportDate;}
+
+
+    }
+
