82da213d9b14cc796d3d2aadae95eb70ed23274e
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-10-11 22:45:08 -0400

    Update DishTest and UserListTest.

--- src/test/java/UserListTest.java
+++ src/test/java/UserListTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class UserListTest {
@@ -9,7 +11,14 @@ public class UserListTest {
 
     @Before
     public void setUp(){
-        userlist = new UserList();
+        HashMap<String, User> userList = new HashMap<String, User>();
+        userList.put("1", new Manager("1", "Chan", "12345"));
+        userList.put("2", new Customer("2", "Howard", "12345"));
+        userList.put("3", new Manager("3", "Nathan", "12345"));
+        userList.put("4", new Customer("4", "George", "12345"));
+        userList.put("5", new Manager("5", "DeDong", "12345"));
+        userList.put("6", new Customer("6", "Eve", "12345"));
+        userList.put("7", new Manager("7", "Raymond", "12345"));userlist = new UserList();
 
     }
 
