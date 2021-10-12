060740042bdf60a7e34d37d065c132ea3ce77bf1
ShaojieDong503 <shaojie.dong@mail.utoronto.ca>
2021-10-12 00:20:17 -0400

    new use case

--- src/main/entities/Product.java
+++ src/main/entities/Product.java
@@ -3,10 +3,24 @@ package entities;
 public abstract class Product extends Inventory{
     public int ExpiryDate;
 
+    /**
+     * Construct an instance of Product
+     * @param name The name of the Inventory
+     * @param usedup The status of wether the Inventory used up or not
+     * @param price The buy in price of the Inventory
+     * @param quantity The quantity of the Inventory
+     * @param ExpiryDate The ExiryDate of the Product
+     */
+
     public Product(String name, boolean usedup, double price, double quantity, int ExpiryDate){
         super(name, usedup, price, quantity);
         this.ExpiryDate = ExpiryDate;
     }
+
+    /**
+     * Get the ExpiryDate of a Product
+     * @return The ExpiryDate of the Product as an int.
+     */
 
     public int getExpiryDate(){
         return this.ExpiryDate;
--- src/main/entities/SoftDrink.java
+++ src/main/entities/SoftDrink.java
@@ -1,6 +1,15 @@
 package entities;
 
 public class SoftDrink extends Product{
+
+    /**
+     * Construct an instance of SoftDrink
+     * @param name The name of the Inventory
+     * @param usedup The status of wether the Inventory used up or not
+     * @param price The buy in price of the Inventory
+     * @param quantity The quantity of the Inventory
+     * @param ExpiryDate The ExiryDate of the Product
+     */
     public SoftDrink(String name, boolean usedup, double price, double quantity, int ExpiryDate){
         super(name, usedup, price, quantity, ExpiryDate);
     }
