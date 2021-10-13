20d6ca26da136bb20a6dfef6da39eae4f6ea5124
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-10-12 21:31:58 -0400

    Reformatting

--- src/test/java/DishListTest.java
+++ src/test/java/DishListTest.java
@@ -3,6 +3,7 @@
  *
  * @author Chan Yu & Naihe Xiao
  */
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,20 +11,31 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DishListTest {
-    DishList dishList;
+    DishList menu = new DishList();
 
     @Before
-    public void setUp(){
-        dishList = new DishList();
-
+    public void setUp() {
+        menu.getDishes().add(new Dish("Quarter pound with cheese", 10.0, new String[]{""}, 200));
+        menu.getDishes().add(new Dish("Quarter pound with 2 cheese", 10.0, new String[]{""}, 400));
+        menu.getDishes().add(new Dish("Quarter pound with 3 cheese", 10.0, new String[]{""}, 500));
+        menu.getDishes().add(new Dish("Quarter pound with 4 cheese", 10.0, new String[]{""}, 600));
+        menu.getDishes().add(new Dish("Quarter pound with 5 cheese", 10.0, new String[]{""}, 700));
+        menu.getDishes().add(new Dish("Small fries", 10.0, new String[]{""}, 200));
+        menu.getDishes().add(new Dish("Median fries", 10.0, new String[]{""}, 250));
+        menu.getDishes().add(new Dish("Large fries", 10.0, new String[]{""}, 350));
+        menu.getDishes().add(new Dish("Coke", 10.0, new String[]{""}, 180));
+        menu.getDishes().add(new Dish("Coffee", 10.0, new String[]{""}, 0));
+        menu.getDishes().add(new Dish("Tea", 10.0, new String[]{""}, 0));
+        menu.getDishes().add(new Dish("Milk Tea", 10.0, new String[]{""}, 300));
+        menu.getDishes().add(new Dish("Wine", 10.0, new String[]{""}, 100));
     }
 
     @Test
-    public void testDishList(){
-        assertEquals(dishList.getDishes().size(), 13);
-        Dish testDish = new Dish("Small fries", 10.0, new String[]{""}, 200 );
-        dishList.getDishes().add(testDish);
-        assertTrue(dishList.getDishes().contains(testDish));
+    public void testDishList() {
+        assertEquals(menu.getDishes().size(), 13);
+        Dish testDish = new Dish("Small fries", 10.0, new String[]{""}, 200);
+        menu.getDishes().add(testDish);
+        assertTrue(menu.getDishes().contains(testDish));
     }
 
 }
--- src/test/java/DishTest.java
+++ src/test/java/DishTest.java
@@ -3,6 +3,7 @@
  *
  * @author Chan Yu & Naihe Xiao
  */
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,20 +14,20 @@ public class DishTest {
     Dish dish2;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         dish1 = new Dish("MaPoTouFu", 12.5, new String[]{"Tofu", "Spice", "Garlic"}, 200);
         dish2 = new Dish("RoYaleWithCheese", 10, new String[]{"Bread", "Ketchup", "Cheese"}, 500);
     }
 
     @Test(timeout = 50)
-    public void testDishConstructor1(){
+    public void testDishConstructor1() {
         assertEquals(dish1.getName(), "MaPoTouFu");
         assertTrue(dish1.getPrice() == 12.5);
         assertTrue(dish2.getIngredients().length == 3);
     }
 
     @Test(timeout = 300)
-    public void testToString(){
+    public void testToString() {
         assertEquals(dish1.toString(), "Dish{name='MaPoTouFu', price=12.5, ingredients=[Tofu, Spice, Garlic], features={}, calories=200.0, allergyInformation='none', status='ordered}");
     }
 
