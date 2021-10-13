b8bd117ce699c91950cf3c79d4097e4a46d27073
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-10-13 10:34:36 -0400

    Update of Dishlist.

--- src/main/DishList.java
+++ src/main/DishList.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @author Chan Yu & Naihe Xiao
  */
 public class DishList {
-    private ArrayList<Dish> menu;
+    private HashMap<String, Dish> menu;
     /* one list may be better? */
 
     /**
@@ -25,21 +25,21 @@ public class DishList {
      * This method is used to generate a list of dishes that used to display to customers/managers.
      * //TODO Later, should be removed since the data should come from DB/external source.
      */
-    public ArrayList<Dish> generateDishList(){
-        ArrayList<Dish> menu = new ArrayList<>();
-        menu.add(new Dish("Quarter pound with cheese", 10.0, new String[]{""}, 200 ));
-        menu.add(new Dish("Quarter pound with 2 cheese", 10.0, new String[]{""}, 400 ));
-        menu.add(new Dish("Quarter pound with 3 cheese", 10.0, new String[]{""}, 500 ));
-        menu.add(new Dish("Quarter pound with 4 cheese", 10.0, new String[]{""}, 600 ));
-        menu.add(new Dish("Quarter pound with 5 cheese", 10.0, new String[]{""}, 700 ));
-        menu.add(new Dish("Small fries", 10.0, new String[]{""}, 200 ));
-        menu.add(new Dish("Median fries", 10.0, new String[]{""}, 250 ));
-        menu.add(new Dish("Large fries", 10.0, new String[]{""}, 350 ));
-        menu.add(new Dish("Coke", 10.0, new String[]{""}, 180 ));
-        menu.add(new Dish("Coffee", 10.0, new String[]{""}, 0 ));
-        menu.add(new Dish("Tea", 10.0, new String[]{""}, 0 ));
-        menu.add(new Dish("Milk Tea", 10.0, new String[]{""}, 300 ));
-        menu.add(new Dish("Wine", 10.0, new String[]{""}, 100 ));
+    public HashMap<String, Dish> generateDishList(){
+        HashMap<String, Dish> menu = new HashMap<String, Dish>();
+        menu.put("Quarter pound with cheese", new Dish("Quarter pound with cheese", 10.0, new HashMap<>(), 200 ));
+        menu.put("Quarter pound with 2 cheese", new Dish("Quarter pound with 2 cheese", 10.0, new HashMap<>(), 400 ));
+        menu.put("Quarter pound with 3 cheese", new Dish("Quarter pound with 3 cheese", 10.0, new HashMap<>(), 500 ));
+        menu.put("Quarter pound with 4 cheese", new Dish("Quarter pound with 4 cheese", 10.0, new HashMap<>(), 600 ));
+        menu.put("Quarter pound with 5 cheese", new Dish("Quarter pound with 5 cheese", 10.0, new HashMap<>(), 700 ));
+        menu.put("Small fries", new Dish("Small fries", 10.0, new HashMap<>(), 200 ));
+        menu.put("Median fries", new Dish("Median fries", 10.0, new HashMap<>(), 250 ));
+        menu.put("Large fries", new Dish("Large fries", 10.0, new HashMap<>(), 350 ));
+        menu.put("Coke", new Dish("Coke", 10.0, new HashMap<>(), 180 ));
+        menu.put("Coffee", new Dish("Coffee", 10.0, new HashMap<>(), 0 ));
+        menu.put("Tea", new Dish("Tea", 10.0, new HashMap<>(), 0 ));
+        menu.put("Milk Tea", new Dish("Milk Tea", 10.0, new HashMap<>(), 300 ));
+        menu.put("Wine", new Dish("Wine", 10.0, new HashMap<>(), 100 ));
         return menu;
     }
 
@@ -48,8 +48,6 @@ public class DishList {
      *
      * @return
      */
-    public List<Dish> getDishes(){
-        return this.menu;
-    }
+
 
 }
