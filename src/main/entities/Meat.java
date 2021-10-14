a3650687378b19b17407352324021227c5fc7cda
ShaojieDong503 <shaojie.dong@mail.utoronto.ca>
2021-10-13 23:25:16 -0400

    tests

--- src/main/entities/Meat.java
+++ src/main/entities/Meat.java
@@ -4,16 +4,15 @@ public class Meat extends Material {
     /**
      * Construct an instance of Meat
      * @param name The name of the Inventory
-     * @param usedup The status of wether the Inventory used up or not
      * @param price The buy in price of the Inventory
      * @param quantity The quantity of the Inventory
      * @param freshness The freshness of the Material
      * @param ImportDate The ImportDate of the Material
      */
 
-    public Meat(String name, boolean usedup, double price, double quantity, String freshness,
+    public Meat(String name, double price, double quantity, String freshness,
                 int ImportDate){
-        super(name, usedup, price, quantity, freshness, ImportDate);
+        super(name, price, quantity, freshness, ImportDate);
 
     }
 
--- src/main/entities/Product.java
+++ src/main/entities/Product.java
@@ -6,14 +6,13 @@ public abstract class Product extends Inventory{
     /**
      * Construct an instance of Product
      * @param name The name of the Inventory
-     * @param usedup The status of wether the Inventory used up or not
      * @param price The buy in price of the Inventory
      * @param quantity The quantity of the Inventory
      * @param ExpiryDate The ExiryDate of the Product
      */
 
-    public Product(String name, boolean usedup, double price, double quantity, int ExpiryDate){
-        super(name, usedup, price, quantity);
+    public Product(String name, double price, double quantity, int ExpiryDate){
+        super(name, price, quantity);
         this.ExpiryDate = ExpiryDate;
     }
 
