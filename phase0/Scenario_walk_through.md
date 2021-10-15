df4eba8d1551a541e6a4cb470fb023c9a6e62a95
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-10-15 16:25:58 -0400

    Update of DishList and scenario.

--- phase0/Scenario_walk_through.md
+++ phase0/Scenario_walk_through.md
@@ -1,9 +1,8 @@
 ## Scenario Walkthrough
 A dine-in UseCase.Customer checks the menu, which obtains a list of dishes and their prices from DishList. When the UseCase.Customer
-places an entities.Order on PlaceOrderQueue, if the Inventory lacks ingredients to make a entities.Dish, then the order is rejected.
-Otherwise, the total price is calculated and returned to the UseCase.Customer.
+places an entities.Order on PlaceOrderQueue, the total price is calculated and returned to the UseCase.Customer.
 
-The kitchen will repeatedly ask for orders to cook, when they finish a dish, they update the ingredient quantity in
+The kitchen will repeatedly ask for orders to be cooked, when they finish a dish, they update the ingredient quantity in
 the inventory, and they check if this dish belongs to a delivery order or a dine-in order. If dine-in order, then add
 a tuple of table number and dish to a queue for the serving staff. Serving staff repeatedly check if there is any
 dish to be served, if yes, they pop the dish and serve it.  
--- src/main/entities/DishList.java
+++ src/main/entities/DishList.java
@@ -1,48 +1,50 @@
-//import java.util.HashMap;
-//
-///**
-// * Public class storing all dishes information using an ArrayList.
-// *
-// * @author Chan Yu & Naihe Xiao
-// */
-//public class DishList {
-//    private HashMap<String, entities.Dish> menu;
-//
-//    /**
-//     * This constructor is using the generateDishList method below which hardcoded the dishes in program.
-//     */
-//    public DishList() {
-//        this.menu = new HashMap<>();
-//    }
-//
-//    public DishList(DishList dishList) {
-//        this.menu = dishList.getDishes();
-//    }
-//
-//
-//    /**
-//     * Return list of dishes
-//     *
-//     * @return
-//     */
-//    public HashMap<String, entities.Dish> getDishes() {
-//        return this.menu;
-//    }
-//
-//
-//    /**
-//     * Override the toString method of Object and return a fine illustration of the DishList information
-//     *
-//     * @return a string representation of the list of Dishes
-//     */
-//    @Override
-//    public String toString() {
-//        int dishNumber = 1;
-//        StringBuilder menuString = new StringBuilder();
-//        for (String dishName : menu.keySet()) {
-//            menuString.append(dishNumber + ". " + menu.get(dishName).toString());
-//            dishNumber++;
-//        }
-//        return menuString.toString();
-//    }
-//}
+package entities;
+
+import java.util.HashMap;
+
+/**
+ * Public class storing all dishes information using an ArrayList.
+ *
+ * @author Chan Yu & Naihe Xiao
+ */
+public class DishList {
+    private HashMap<String, entities.Dish> menu;
+
+    /**
+     * This constructor is using the generateDishList method below which hardcoded the dishes in program.
+     */
+    public DishList() {
+        this.menu = new HashMap<>();
+    }
+
+    public DishList(DishList dishList) {
+        this.menu = dishList.getDishes();
+    }
+
+
+    /**
+     * Return list of dishes
+     *
+     * @return
+     */
+    public HashMap<String, entities.Dish> getDishes() {
+        return this.menu;
+    }
+
+
+    /**
+     * Override the toString method of Object and return a fine illustration of the DishList information
+     *
+     * @return a string representation of the list of Dishes
+     */
+    @Override
+    public String toString() {
+        int dishNumber = 1;
+        StringBuilder menuString = new StringBuilder();
+        for (String dishName : menu.keySet()) {
+            menuString.append(dishNumber + ". " + menu.get(dishName).toString());
+            dishNumber++;
+        }
+        return menuString.toString();
+    }
+}
