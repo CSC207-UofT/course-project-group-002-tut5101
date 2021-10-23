e9a22438a73121cb5cf990c44b7e704ab693c3aa
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-10-23 10:21:27 -0400

    Created manager user_case

--- src/main/use_case/ManagerUseCase.java
+++ src/main/use_case/ManagerUseCase.java
@@ -1,4 +1,82 @@
 package use_case;
 
+import entity.Manager;
+import entity.Review;
+import entity.Staff;
+
 public class ManagerUseCase {
+
+    /**
+     * Manager issue payment to a specific staff.
+     *
+     * @param manager the current manager in system
+     * @param staff   the staff who are getting payment
+     * @return true iff the payment issue succeed
+     */
+    public boolean payStaffSalary(Manager manager, Staff staff) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * // TODO not clear what the staff be assigned for?
+     * Manager assign
+     *
+     * @param manager the current manager in system
+     * @param staff   the new staff who will be assigned
+     * @return true iff the assignment succeed
+     */
+    public boolean assignServingStaff(Manager manager, Staff staff) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * Manager process enrollment of new staff.
+     *
+     * @param manager the current manager in system
+     * @param staff   the new staff who will be enrolled
+     * @return true iff the enrollment process success
+     */
+    public boolean enrollStaff(Manager manager, Staff staff) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * Manager adjust the menu(dish list).
+     *
+     * @param manager the current manager in system
+     * @param menu    the current menu in system
+     * @return true     iff the adjustment is made successfully
+     */
+    public boolean manageMenu(Manager manager, DishList menu) {
+        //TODO
+        return false;
+    }
+
+
+    /**
+     * Manager request out of stock status.
+     *
+     * @param manager       the current manager in system
+     * @param inventoryList the current inventory status in system
+     * @return true iff the request is made successfully
+     */
+    public boolean requestInventory(Manager manager, InventoryList inventoryList) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * @param manager the current manager in system
+     * @param review  the review that will be updated
+     * @return true iff the adjustment is made successfully
+     */
+    public boolean adjustReview(Manager manager, Review review) {
+        //TODO
+        return false;
+    }
+
+
 }
