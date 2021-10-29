0771b05ff84d8b55a178258734f4eb858b8e8667
ShaojieDong503 <shaojie.dong@mail.utoronto.ca>
2021-10-28 23:33:48 -0400

    csv readwriter

--- src/main/entity/Alcohol.java
+++ src/main/entity/Alcohol.java
@@ -1,16 +1,78 @@
 package entity;
 
-public class Alcohol extends Product{
+import java.io.Serializable;
+
+public class Alcohol implements Inventory,HasExpiry, Serializable {
+    public String name;
+    protected boolean usedup;
+    protected double price;
+    protected double quantity;
+    public final int ExpiryDate;
 
     /**
-     * Construct an instance of Alcohol
+     * Construct an instance of Product
      * @param name The name of the Inventory
      * @param price The buy in price of the Inventory
      * @param quantity The quantity of the Inventory
      * @param ExpiryDate The ExiryDate of the Product
      */
+
     public Alcohol(String name, double price, double quantity, int ExpiryDate){
-        super(name, price, quantity, ExpiryDate);
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.ExpiryDate = ExpiryDate;
+        this.usedup = quantity == 0;
+    }
+    @Override
+    public String getName(){return this.name;}
+
+    @Override
+    public boolean getUsedup(){
+        return this.usedup;
+    }
+
+
+    /**
+     * Get the quantity of an Inventory
+     * @return The quantity of the Inventory as a double.
+     */
+    @Override
+    public double getQuantity(){
+        return this.quantity;
+    }
+
+    /**
+     * Check wether have enough Inventory for use and change the amount of it
+     * @param usage The required amount of usage of this inventory
+     * @return Wether the inventory is enough to use.
+     */
+    @Override
+    public void updateQuantity(double usage){
+        if(this.quantity >= usage){
+            this.quantity -= usage;
+            if(this.quantity == 0){
+                this.usedup = true;
+            }
+        }
+        else{System.out.println("Not enough");}
+    }
+
+
+    /**
+     * Get the ExpiryDate of a Product
+     * @return The ExpiryDate of the Product as an int.
+     */
+    @Override
+    public int getExpiryDate(){
+        return this.ExpiryDate;
+    }
+
+    @Override
+    public String toString(){
+        String s = "alcohol;"+this.name+ ";"+ this.price +
+                ";"+ this.quantity +";"+ this.ExpiryDate;
+        return s;
 
     }
 }
--- src/main/entity/SoftDrink.java
+++ src/main/entity/SoftDrink.java
@@ -1,15 +1,79 @@
 package entity;
 
-public class SoftDrink extends Product{
+import java.io.Serializable;
+
+public class SoftDrink implements Inventory, HasExpiry, Serializable {
+    public String name;
+    protected boolean usedup;
+    protected double price;
+    protected double quantity;
+    public final int ExpiryDate;
 
     /**
-     * Construct an instance of SoftDrink
+     * Construct an instance of Product
      * @param name The name of the Inventory
      * @param price The buy in price of the Inventory
      * @param quantity The quantity of the Inventory
      * @param ExpiryDate The ExiryDate of the Product
      */
+
     public SoftDrink(String name, double price, double quantity, int ExpiryDate){
-        super(name, price, quantity, ExpiryDate);
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.ExpiryDate = ExpiryDate;
+        this.usedup = quantity == 0;
+    }
+
+    @Override
+    public String getName(){return this.name;}
+
+    @Override
+    public boolean getUsedup(){
+        return this.usedup;
+    }
+
+
+    /**
+     * Get the quantity of an Inventory
+     * @return The quantity of the Inventory as a double.
+     */
+    @Override
+    public double getQuantity(){
+        return this.quantity;
+    }
+
+    /**
+     * Check wether have enough Inventory for use and change the amount of it
+     * @param usage The required amount of usage of this inventory
+     * @return Wether the inventory is enough to use.
+     */
+    @Override
+    public void updateQuantity(double usage){
+        if(this.quantity >= usage){
+            this.quantity -= usage;
+            if(this.quantity == 0){
+                this.usedup = true;
+            }
+        }
+        else{System.out.println("Not enough");}
+    }
+
+
+    /**
+     * Get the ExpiryDate of a Product
+     * @return The ExpiryDate of the Product as an int.
+     */
+    @Override
+    public int getExpiryDate(){
+        return this.ExpiryDate;
+    }
+
+    @Override
+    public String toString(){
+        String s = "softdrink;"+this.name+ ";"+ this.price +
+                ";"+ this.quantity +";"+ this.ExpiryDate;
+        return s;
+
     }
 }
