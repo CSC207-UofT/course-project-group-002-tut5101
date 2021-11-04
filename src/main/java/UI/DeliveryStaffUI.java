5de851317e063211cb6f24c35aa7fb2528aeaf26
ddx <xiededong001211@gmail.com>
2021-11-04 14:20:30 -0400

    Update DeliveryStaffUI, create separate files for StaffUIMessage and LoginLogoutUIMessage

--- src/main/java/UI/DeliveryStaffUI.java
+++ src/main/java/UI/DeliveryStaffUI.java
@@ -1,6 +1,7 @@
 package UI;
 
-import constant.UIMessage;
+import constant.StaffUIMessage;
+import constant.LoginLogoutUIMessage;
 import controller.StaffController;
 
 import java.util.Scanner;
@@ -19,15 +20,23 @@ public class DeliveryStaffUI implements UserInterface {
     public void loadUi (String id) {
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println(UIMessage.GREETING_ASK_FOR_ACTION + UIMessage.DELIVERY_STAFF_ACTIONS);
+            System.out.println(StaffUIMessage.GREETING_ASK_FOR_ACTION + StaffUIMessage.DELIVERY_STAFF_ACTIONS);
             int action = scanner.nextInt();
             switch (action) {
                 case 0:
                     // Case 0: logout
-                    System.out.println("You have successfully logged out");
+                    System.out.println(LoginLogoutUIMessage.LOGOUT_SUCCESSFUL);
                     return;
                 case 1:
-                    // Case 1: get next order
+                    // Case 1: view current order
+                    try {
+                        System.out.println(currentDeliveryStaff.displayCurrent(id));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 2:
+                    // Case 2: get next order
                     try {
                         currentDeliveryStaff.getNext(id);
                         System.out.println(currentDeliveryStaff.displayCurrent(id));
@@ -35,11 +44,11 @@ public class DeliveryStaffUI implements UserInterface {
                         System.out.println(e.getMessage());
                     }
                     break;
-                case 2:
-                    // Case 2: mark current as delivered
+                case 3:
+                    // Case 3: mark current as delivered
                     try {
                         currentDeliveryStaff.completeCurrent(id);
-                        System.out.println("Order marked as completed\n");
+                        System.out.println(StaffUIMessage.ORDER_MARKED_COMPLETED);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
