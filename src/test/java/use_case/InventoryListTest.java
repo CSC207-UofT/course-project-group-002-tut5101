63ba7a32a1405ea00036b2614149d9edee7a15b7
MeeskaM00ska <nathannnnn.li@mail.utoronto.ca>
2021-11-05 00:08:39 -0400

    simple test cases for inventortlist.

---
+++ src/test/java/use_case/InventoryListTest.java
@@ -0,0 +1,32 @@
+package use_case;
+/**
+ * Tests for InventoryList Class
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InventoryListTest {
+    InventoryList invList = new InventoryList();
+
+    @Before
+    public void setUp() {
+        invList.addInventory(new entity.Grain("corn", 0.99, 699, "a",20221122));
+        invList.addInventory(new entity.Meat("pork",5.00,100,"b",20211030));
+        invList.addInventory(new entity.Seafood("samon fish",10.99,23,"c",20211029));
+        invList.addInventory(new entity.Alcohol("wine",69.99,4,20221006));
+    }
+
+    @Test
+    public void testCheckExist1() {assertEquals(invList.checkExist("corn"), true);}
+
+    @Test
+    public void testCheckExist2() {assertEquals(invList.checkExist("wine"), true);}
+
+    @Test
+    public void testCheckExist3() {assertEquals(invList.checkExist("a"), false);}
+
+
+}
