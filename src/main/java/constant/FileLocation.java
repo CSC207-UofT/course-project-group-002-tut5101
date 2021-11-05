99cce694cf6864d38a0d88f4ed620edd1aa33137
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-11-05 15:43:40 -0400

    update FileLocation

--- src/main/java/constant/FileLocation.java
+++ src/main/java/constant/FileLocation.java
@@ -1,9 +1,9 @@
 package constant;
 
 public class FileLocation {
-    public static final String MENU_FILE_LOCATION = "src/main/resources/resources";
+    public static final String MENU_FILE_LOCATION = "src/main/resources/menu.ser";
     public static final String USER_FILE_LOCATION = "src/main/resources/users.ser";
-    public static final String INVENTORY_FILE_LOCATION = "";
-    public static final String REVIEW_LIST_LOCATION = "";
+    public static final String INVENTORY_FILE_LOCATION = "src/main/resources/inventory.ser";
+    public static final String REVIEW_LIST_LOCATION = "src/main/resources/review.ser";
 
 }
