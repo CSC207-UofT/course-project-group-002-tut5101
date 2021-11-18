62a11f72b00dc55c9d0a27bd4e2e89d4597c61d1
ShaojieDong503 <shaojie.dong@mail.utoronto.ca>
2021-11-18 14:35:04 -0500

    gateway testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,10 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        while (true) {
-            RestaurantSystem system = new RestaurantSystem();
-            system.run();
-        }
+       // while (true) {
+       //     RestaurantSystem system = new RestaurantSystem();
+       //     system.run();
+       // }
+        DishList menu = new DishList("src/main/resources/menu.ser");
+        System.out.println(menu.size());
 
     }
 }
