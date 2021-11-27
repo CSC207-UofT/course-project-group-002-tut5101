d10c088ffcf89ae7ae204132a291f4d049aed23c
MeeskaM00ska <nathannnnn.li@mail.utoronto.ca>
2021-11-26 20:02:05 -0500

    gui for add review

---
+++ Android/app/src/main/java/com/example/androidgui/Review/AddRateActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidgui.Review;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rate);
+    }
+}
