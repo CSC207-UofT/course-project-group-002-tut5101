ddf21def9ed62c15cc1b2fa6f81a0a10afd99af4
ddx <xiededong001211@gmail.com>
2021-11-27 21:32:56 -0500

    Add activities for select action for staffs

---
+++ Android/app/src/main/java/com/example/androidgui/DeliverOrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidgui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeliverOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deliver_order);
+    }
+
+
+}
