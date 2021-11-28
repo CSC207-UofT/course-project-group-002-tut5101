4648a75fe5c44ec907462c77f60808d52c63510c
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-11-28 08:23:19 -0500

    comment out UI code in phase1

--- Android/app/src/main/java/ui/loginPrompt/LoginUI.java
+++ Android/app/src/main/java/ui/loginPrompt/LoginUI.java
@@ -24,22 +24,23 @@ public class LoginUI {
         System.out.println(LoginLogoutUIMessage.ASK_FOR_PASSWORD);
         String password = scanner.nextLine();
 
-        switch (controller.runLogin(id, password)) {
-            case NO_SUCH_USER:
-                System.out.println(LoginLogoutUIMessage.ASK_FOR_SIGNUP);
-                System.out.println("Please give your id, name and password in format of id,name,password eg. 2,Ben,12345");
-                if (scanner.hasNextLine()) {
-                    String str = scanner.nextLine();
-                    System.out.println(controller.RegisterUser(str));
-                }
-                return null;
-            case FAILURE:
-                System.out.println(LoginLogoutUIMessage.LOGIN_FAILED);
-                return null;
-            case SUCCESS:
-                return id;
-            default:
-                return null;
-        }
+//        switch (controller.runLogin(id, password)) {
+//            case NO_SUCH_USER:
+//                System.out.println(LoginLogoutUIMessage.ASK_FOR_SIGNUP);
+//                System.out.println("Please give your id, name and password in format of id,name,password eg. 2,Ben,12345");
+//                if (scanner.hasNextLine()) {
+//                    String str = scanner.nextLine();
+////                    System.out.println(controller.RegisterUser(str));
+//                }
+//                return null;
+//            case FAILURE:
+//                System.out.println(LoginLogoutUIMessage.LOGIN_FAILED);
+//                return null;
+//            case SUCCESS:
+//                return id;
+//            default:
+//                return null;
+//        }
+        return "";
     }
 }
