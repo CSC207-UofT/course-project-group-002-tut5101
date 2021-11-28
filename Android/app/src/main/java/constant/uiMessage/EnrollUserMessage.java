1d62f7c801a36a4cb842e1e2440098450cc47ae1
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-11-28 01:36:24 -0500

    String values for enroll staff w/ Android

---
+++ Android/app/src/main/java/constant/uiMessage/EnrollUserMessage.java
@@ -0,0 +1,9 @@
+package constant.uiMessage;
+
+public class EnrollUserMessage {
+
+    public static final String INFO_REQUIRED = "Info required!";
+    public static final String PASSWORD_NOT_MATCH = "Password not match";
+    public static final String PASSWORD_NOT_MATCH_MESSAGE = "The password is not matching, please re-enter.";
+    public static final String ENROLL_SUCCEED = "Succeed! The new staff is enrolled.";
+}
