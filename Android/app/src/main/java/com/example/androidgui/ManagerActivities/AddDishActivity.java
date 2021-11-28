2927c8d76683a464596e8c48ff68bda25485bf8a
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-11-28 18:18:42 -0500

    Update of functionality of manager.

---
+++ Android/app/src/main/java/com/example/androidgui/ManagerActivities/AddDishActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidgui.ManagerActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.androidgui.R;
+
+public class AddDishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dish);
+    }
+}
--- Android/app/src/main/java/com/example/androidgui/ManagerActivities/ManageMenuActivity.java
+++ Android/app/src/main/java/com/example/androidgui/ManagerActivities/ManageMenuActivity.java
@@ -26,7 +26,6 @@ public class ManageMenuActivity extends AppCompatActivity {
     String[] managerDecision;
     MenuController menuController = new MenuController();
 
-
     /**
      * Activity basic function.
      *
