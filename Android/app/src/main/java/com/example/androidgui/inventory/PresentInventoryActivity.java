9cec3c95593e61ee2550b567128781a920f74b22
Evelyn <evelyn.chou@mail.utoronto.ca>
2021-11-29 17:26:11 -0500

    moved inventory activities into a package, removed warnings

--- Android/app/src/main/java/com/example/androidgui/inventory/PresentInventoryActivity.java
+++ Android/app/src/main/java/com/example/androidgui/inventory/PresentInventoryActivity.java
@@ -1,10 +1,11 @@
-package com.example.androidgui;
+package com.example.androidgui.inventory;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.androidgui.R;
 
 public class PresentInventoryActivity extends AppCompatActivity{
 
