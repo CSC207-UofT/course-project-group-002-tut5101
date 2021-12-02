3a0e35bb7f06e7216f85378cdfd2c4ef9ebe8a16
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-12-02 16:54:23 -0500

    rename and add javadoc

--- Android/app/src/main/java/use_case/enrollstaff/EnrollUserOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.enrollstaff;
-
-public interface EnrollUserOutputBoundary {
-    void setNewUserId(String id);
-
-    void setAvailUserTypeOptions(String[] staffTypes, int maxValue);
-}
