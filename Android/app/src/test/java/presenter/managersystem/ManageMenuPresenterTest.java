a751f73d734c639bf96e59ce45e0e84628cf934d
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-03 15:44:02 -0500

    Structure of test.

---
+++ Android/app/src/test/java/presenter/managersystem/ManageMenuPresenterTest.java
@@ -0,0 +1,58 @@
+package presenter.managersystem;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test the ManagerMenuPresenter class.
+ */
+public class ManageMenuPresenterTest {
+
+    private ManageMenuPresenter manager;
+
+    /**
+     * Setup before tests
+     */
+    @Before
+    public void setUp() {
+        manager = new ManageMenuPresenter();
+        TestClass testPresenter = new TestClass();
+        manager.setManageMenuViewInterface(testPresenter);
+    }
+
+    /**
+     * Test the getDish method
+     */
+    @Test
+    public void testGetDish(){
+        manager.getDish("2");
+        manager.getDish("1");
+    }
+
+
+    /**
+     * Empty fake presenter class implementing the output boundary
+     */
+    private static class TestClass implements ManageMenuViewInterface {
+
+        /**
+         * Testing use.
+         * @param dishName name of the dish
+         */
+        @Override
+        public void getDish(String dishName) {
+            assert true;
+        }
+
+        /**
+         * Testing use.
+         * @param s array of string.
+         */
+        @Override
+        public void gettingDishList(String[] s) {
+            assert true;
+        }
+    }
+
+}
