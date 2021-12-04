2f7cdef495975e045ee82217795b13b70a28418d
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-04 11:24:42 -0500

    Login presenter test implemented.

---
+++ Android/app/src/test/java/presenter/customer_system/LoginPresenterTest.java
@@ -0,0 +1,74 @@
+package presenter.customer_system;
+
+import constant.manger_system.UserType;
+import constant.ui_message.LoginResult;
+import org.junit.Before;
+import org.junit.Test;
+import presenter.login_system.LoginPresenter;
+import presenter.login_system.LoginViewInterface;
+
+public class LoginPresenterTest {
+
+    private LoginPresenter login;
+
+    /**
+     * Setup before tests
+     */
+    @Before
+    public void setUp() {
+        login = new LoginPresenter();
+        TestClass testPresenter = new TestClass();
+        login.setViewInterface(testPresenter);
+    }
+
+    /**
+     * Test the runLogin method
+     */
+    @Test
+    public void runLogin(){
+        login.runLogin("1","23");
+    }
+
+    /**
+     * Test the getUserTypeById method
+     */
+    @Test
+    public void getUserTypeById(){
+        login.getUserTypeById("2");
+    }
+
+    /**
+     * Test the getUserTypeById method
+     */
+    @Test
+    public void presentLoginResult(){
+        login.presentLoginResult(LoginResult.SUCCESS);
+    }
+
+    /**
+     * Empty fake presenter class implementing the view interface
+     */
+    private static class TestClass implements LoginViewInterface {
+
+        /**
+         * testing use
+         *
+         * @param result login result
+         */
+        @Override
+        public void presentLoginResult(LoginResult result) {
+            assert true;
+        }
+
+        /**
+         * testing use
+         *
+         * @param userType user type
+         */
+        @Override
+        public void setUserType(UserType userType) {
+            assert true;
+        }
+    }
+
+}
