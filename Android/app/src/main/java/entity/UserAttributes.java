6da1c338d269086515d52b6d6f2bde81b22d5317
ddx <xiededong001211@gmail.com>
2021-12-04 09:38:19 -0500

    Implement private class design pattern on class User to protect user attributes from changing

---
+++ Android/app/src/main/java/entity/UserAttributes.java
@@ -0,0 +1,40 @@
+package entity;
+
+/**
+ * Implement private class design pattern to reduce exposure of central user data
+ */
+public class UserAttributes {
+    private String id;
+    private String name;
+    private String password;
+
+    /**
+     * Constructor for user attributes
+     * @param id The id of a user
+     * @param name The name of a user
+     * @param password The password of a user
+     */
+    public UserAttributes(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    /**
+     * Get the id of a user
+     * @return id of the user
+     */
+    public String getId() { return this.id; }
+
+    /**
+     * Get the name of the user
+     * @return name of the user
+     */
+    public String getName() { return this.name; }
+
+    /**
+     * Get the password of the user
+     * @return password of the user
+     */
+    public String getPassword() { return this.password; }
+}
