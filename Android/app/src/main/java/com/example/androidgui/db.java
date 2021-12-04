03c6fdd076d57a258ed4456a9c5725215c9574ce
Raymond <basmaskate@gmail.com>
2021-12-04 14:14:30 -0500

    Success in data read write on google cloud storage

--- Android/app/src/main/java/com/example/androidgui/db.java
+++ Android/app/src/main/java/com/example/androidgui/db.java
@@ -10,13 +10,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import com.google.android.gms.common.util.IOUtils;
+import com.google.api.gax.paging.Page;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
+import com.google.common.collect.Lists;
 import gateway.FetchData;
 import gateway.PutData;
 
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -31,18 +31,14 @@ public class db extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        try {
+//            authExplicit();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        readFile();
 
-        Uri otherPath = Uri.parse("/Users/liuray/Downloads/uplifted-cinema-252918-e0674925237b.json");
-        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS",otherPath.toString());
-
-
-
-
-        try {
-            uploadObject("uplifted-cinema-252918", "207project", "WQ11.txt", "asdfkl");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -54,22 +50,29 @@ public class db extends AppCompatActivity {
                 StringBuilder urls = new StringBuilder();
                 try {
                     // Create a URL for the desired page
-                    URL url = new URL("https://storage.googleapis.com/207project/WQ11.txt"); //My text file location
+                    URL url = new URL("https://storage.googleapis.com/207project/sample.ser"); //My text file location
                     //First open the connection
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setConnectTimeout(60000); // timing out in a minute
 
-                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//
+//                    String str;
+//                    while ((str = in.readLine()) != null) {
+//                        urls.append(str);
+//                    }
+//                    System.out.println("READING:" + urls);
+//                    in.close();
+                    InputStream file = conn.getInputStream();
+                    InputStream buffer = new BufferedInputStream(file);
+                    ObjectInput input = new ObjectInputStream(buffer);
 
-                    String str;
-                    while ((str = in.readLine()) != null) {
-                        urls.append(str);
-                    }
-                    System.out.println(urls);
-                    in.close();
+                    // serialize the Map
+                    HashMap result = (HashMap) input.readObject();
+                    System.out.println("READ RESULT:" + result);
+                    input.close();
 
-
-                } catch (Exception e) {
+                } catch (IOException | ClassNotFoundException e) {
                     Log.d("MyTag", e.toString());
                 }
 
@@ -108,13 +111,18 @@ public class db extends AppCompatActivity {
      * @param filePath path to the file
      * @throws IOException
      */
-    public static void uploadObject(
+    public void uploadObject(
             String projectId, String bucketName, String objectName, String filePath) throws IOException {
 
         new Thread(new Runnable() {
 
             @Override
             public void run() {
+                try {
+                    authExplicit();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("did1");
                 Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
                 System.out.println("did2");
@@ -133,6 +141,48 @@ public class db extends AppCompatActivity {
 //                        System.out.println(urls);
 //                    }
 //                });
+            }
+        }).start();
+    }
+
+    void authExplicit() throws IOException {
+        // You can specify a credential file by providing a path to GoogleCredentials.
+        // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                GoogleCredentials credentials = null;
+                try {
+                    credentials = GoogleCredentials.fromStream(getAssets().open("uplifted-cinema-252918-80dc77e32e4e.json"))
+                            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+                    Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+                    BlobId blobId = BlobId.of("207project", "sample.ser");
+                    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+
+                    HashMap<Integer, Integer> test = new HashMap<>();
+                    test.put(1, 2);
+                    test.put(2, 4);
+                    test.put(3, 9);
+                    test.put(4, 16);
+                    test.put(5, 25);
+
+                    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+                    ObjectOutputStream out = new ObjectOutputStream(byteOut);
+                    out.writeObject(test);
+                    System.out.println("BYTE:" + byteOut);
+                    storage.create(blobInfo, byteOut.toByteArray());
+
+//                    String test = "asdfasfdjkalsdfklasdjkfalsdfjkasldfjaklsdjfkalsdf";
+//                    storage.create(blobInfo, test.getBytes());
+//                    System.out.println("WRITING:" + test.getBytes());
+                    // storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
+                    // storage.create(blobInfo, Files.readAllBytes(getAssets().open("inventory.ser")));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
     }
