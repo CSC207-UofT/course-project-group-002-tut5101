22c21518249fffc7665aeece0b81deb066d759e5
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-04 11:41:25 -0500

    UpdateFreshness presenter test implemented.

---
+++ Android/app/src/test/java/presenter/inventory_system/UpdateFreshnessPresenterTest.java
@@ -0,0 +1,45 @@
+package presenter.inventory_system;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Testing the CheckInventoryPresenter.
+ */
+public class UpdateFreshnessPresenterTest {
+    private UpdateFreshnessPresenter update;
+
+    /**
+     * Setup before tests
+     */
+    @Before
+    public void setUp() {
+        update = new UpdateFreshnessPresenter();
+        TestClass testPresenter = new TestClass();
+        update.setUpdateFreshnessInterface(testPresenter);
+    }
+
+    /**
+     * Test the popInfo method
+     */
+    @Test
+    public void popInfo(){
+        update.popInfo("1","2");
+    }
+
+    /**
+     * Empty fake presenter class implementing the view interface
+     */
+    private static class TestClass implements UpdateFreshnessInterface  {
+
+        /**
+         * test use
+         *
+         * @param message message
+         */
+        @Override
+        public void popInfo(String message) {
+            assert true;
+        }
+    }
+}
