820f3f6b84e6fc775e8f459122a2438b68ab97e7
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-12-04 21:48:03 -0500

    complete LoginPresenterTest

--- Android/app/src/test/java/presenter/staff_system/LoginPresenterTest.java
+++ Android/app/src/test/java/presenter/staff_system/LoginPresenterTest.java
@@ -2,17 +2,26 @@ package presenter.staff_system;
 
 import constant.manger_system.UserType;
 import constant.ui_message.LoginResult;
+import entity.customer.Customer;
+import entity.delivery.DeliveryStaff;
+import entity.delivery.ServingStaff;
+import entity.inventory.InventoryStaff;
+import entity.kitchen.KitchenStaff;
+import entity.manager.Manager;
 import org.junit.Before;
 import org.junit.Test;
 import presenter.login_system.LoginPresenter;
 import presenter.login_system.LoginViewInterface;
+import use_case.user_list.UserList;
 
+import static org.junit.Assert.assertEquals;
 /**
  * Testing the login presenter.
  */
 public class LoginPresenterTest {
 
     private LoginPresenter login;
+    private UserList userList;
 
     /**
      * Setup before tests
@@ -22,6 +31,7 @@ public class LoginPresenterTest {
         login = new LoginPresenter();
         TestClass testPresenter = new TestClass();
         login.setViewInterface(testPresenter);
+        generateUserList();
     }
 
     /**
@@ -45,8 +55,22 @@ public class LoginPresenterTest {
      */
     @Test
     public void presentLoginResult(){
-        login.presentLoginResult(LoginResult.SUCCESS);
+        login.presentLoginResult(LoginResult.FAILURE);
     }
+
+    /**
+     * Generate list of users for testing
+     */
+    private void generateUserList() {
+        userList = new UserList(6);
+        userList.addUser(new Manager());
+        userList.addUser(new Customer("1", "James", "12345"));
+        userList.addUser(new DeliveryStaff("2", "Amy", "12345"));
+        userList.addUser(new ServingStaff("3", "Eve", "12345"));
+        userList.addUser(new KitchenStaff("4", "Bob", "12345"));
+        userList.addUser(new InventoryStaff("5", "Frank", "12345"));
+    }
+
 
     /**
      * Empty fake presenter class implementing the view interface
@@ -60,7 +84,7 @@ public class LoginPresenterTest {
          */
         @Override
         public void presentLoginResult(LoginResult result) {
-            assert true;
+            assertEquals(result, LoginResult.FAILURE);
         }
 
         /**
@@ -70,7 +94,7 @@ public class LoginPresenterTest {
          */
         @Override
         public void setUserType(UserType userType) {
-            assert true;
+            assertEquals(userType, UserType.DELIVERY_STAFF);
         }
     }
 
