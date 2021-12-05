6501b61bfd63540dcc0df00bb89a9d92eb3bc0e8
ddx <xiededong001211@gmail.com>
2021-12-05 18:09:26 -0500

    Delete not used file

--- Android/app/src/main/java/use_case/deliver_order/ServeDishInterface.java
+++
@@ -1,7 +0,0 @@
-package use_case.deliver_order;
-
-public interface ServeDishInterface {
-    void delivered(String id) throws Exception;
-    void getToBeDeliver(String id) throws Exception;
-    String getItemInfo(String id);
-}
