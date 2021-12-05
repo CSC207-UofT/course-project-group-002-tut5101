f8ad379a021c36f8ae163440ad5664e4efce79ad
Chan Yu <chanxq.yu@mail.utoronto.ca>
2021-12-04 22:00:38 -0500

    deleted unused constant

--- Android/app/src/main/java/constant/ui_message/LoginLogoutUIMessage.java
+++ Android/app/src/main/java/constant/ui_message/LoginLogoutUIMessage.java
@@ -5,18 +5,10 @@ package constant.ui_message;
  */
 
 public class LoginLogoutUIMessage {
-    public static final String ASK_FOR_ID = "Please enter your id: ";
-    public static final String ASK_FOR_PASSWORD = "Please enter your password: ";
-    public static final String ASK_FOR_SIGNUP = "Do you want to sign up？";
-    public static final String LOGOUT_SUCCESSFUL = "You have successfully logged out";
-
-
-
     public static final String LOGIN_FAILED = "Login Failed";
     public static final String LOGIN_FAILED_MESSAGE = "Please enter the correct password.";
     public static final String LOGIN_SUCCEED = "Login Succeed!";
     public static final String OK = "OK";
     public static final String STAFF_SIGNUP = "Your ID is not in the system yet, please ask Manager for help.";
-
     public static final String SIGN_UP = "Sign Up?";
 }
