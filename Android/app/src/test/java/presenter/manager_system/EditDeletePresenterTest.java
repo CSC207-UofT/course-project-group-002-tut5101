e9a2f0111465221e71166d65c2506a2caa90b726
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-06 16:45:59 -0500

    Testing new presenter.

--- Android/app/src/test/java/presenter/manager_system/EditDeletePresenterTest.java
+++ Android/app/src/test/java/presenter/manager_system/EditDeletePresenterTest.java
@@ -1,45 +1,57 @@
 package presenter.manager_system;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class EditDeletePresenterTest {
-//    /**
-//     * Test the dishesInMenuAsString method
-//     */
-//    @Test
-//    public void deleteDishByName(){
-//        menuPresenter.deleteDishByName("dish2");
-//    }
-//
-//    /**
-//     * Test the increasePrice method
-//     */
-//    @Test
-//    public void increasePrice(){
-//        menuPresenter.increasePrice("dish2");
-//    }
-//
-//    /**
-//     * Test the decreasePrice method
-//     */
-//    @Test
-//    public void decreasePrice(){
-//        menuPresenter.decreasePrice("dish2");
-//    }
-//
-//    /**
-//     * Test the decreaseCalories
-//     */
-//    @Test
-//    public void decreaseCalories(){
-//        menuPresenter.decreaseCalories("dish2");
-//    }
-//
-//    /**
-//     * Test the increaseCalories
-//     */
-//    @Test
-//    public void increaseCalories(){
-//        menuPresenter.increaseCalories("dish2");
-//    }
+
+    private EditDeletePresenter deleteDishByName;
+
+    /**
+     * Setup before tests
+     */
+    @Before
+    public void setUp() {
+        deleteDishByName = new EditDeletePresenter();
+            }
+
+    /**
+     * Test the dishesInMenuAsString method
+     */
+    @Test
+    public void deleteDishByName(){
+        deleteDishByName.deleteDishByName("dish2");
+    }
+
+    /**
+     * Test the increasePrice method
+     */
+    @Test
+    public void increasePrice(){
+        deleteDishByName.increasePrice("dish2");
+    }
+
+    /**
+     * Test the decreasePrice method
+     */
+    @Test
+    public void decreasePrice(){
+        deleteDishByName.decreasePrice("dish2");
+    }
+
+    /**
+     * Test the decreaseCalories
+     */
+    @Test
+    public void decreaseCalories(){
+        deleteDishByName.decreaseCalories("dish2");
+    }
+
+    /**
+     * Test the increaseCalories
+     */
+    @Test
+    public void increaseCalories(){
+        deleteDishByName.increaseCalories("dish2");
+    }
 }
