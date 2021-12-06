2ce0819d813242d4b7f98744fd3b9a5a281f6310
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-06 18:41:32 -0500

    Testing new presenter.

--- Android/app/src/main/java/presenter/menu_system/SelectEditViewInterface.java
+++ Android/app/src/main/java/presenter/menu_system/SelectEditViewInterface.java
@@ -1,6 +1,10 @@
 package presenter.menu_system;
+
+import android.view.View;
+
 /**
  * Interface for dish selection or edition.
  */
 public interface SelectEditViewInterface {
+    void manageMenu(View v);
 }
