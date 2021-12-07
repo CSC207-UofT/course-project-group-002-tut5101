817e8fcab1ca8edad5a3250f9900c9ac02442dd4
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-07 12:04:23 -0500

    Implementation editing calories functionality.

--- Android/app/src/main/java/com/example/androidgui/menu_activities/EditCaloriesActivity.java
+++ Android/app/src/main/java/com/example/androidgui/menu_activities/EditCaloriesActivity.java
@@ -1,17 +1,83 @@
 package com.example.androidgui.menu_activities;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.androidgui.R;
+import constant.manager_system.ManagerDecision;
+import constant.manager_system.ManagerUIMessage;
+
+import java.util.Objects;
 
 /**
  * Activity class for editing calories
  */
 public class EditCaloriesActivity extends AppCompatActivity {
 
+    private NumberPicker select;
+    private TextView askSelection;
+    private String[] selectOption;
+
+    /**
+     * Activity basic function.
+     *
+     * @param savedInstanceState onCreate fixed parameter.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_calories);
+        select = findViewById(R.id.selectIncreaseOrDecrease);
+        askSelection = findViewById(R.id.askSelection);
+
+        setupMessage();
+        setupOptions();
     }
+
+    /**
+     * Setting up message.
+     */
+    private void setupMessage() {
+        askSelection.setText(ManagerUIMessage.IncreaseDecrease);
+    }
+
+    /**
+     * Setting up options.
+     */
+    private void setupOptions() {
+        selectOption = new String[]{ManagerDecision.ONE.toString(),
+                ManagerDecision.TWO.toString(),
+                ManagerDecision.THREE.toString(),
+                ManagerDecision.FOUR.toString(),
+                ManagerDecision.FIVE.toString(),
+                ManagerDecision.SIX.toString(),};
+        select.setMaxValue(selectOption.length - 1);
+        select.setMinValue(0);
+        select.setDisplayedValues(selectOption);
+    }
+
+    /**
+     * Next activity
+     * @param v v
+     */
+    public void confirm(View v){
+        String action = selectOption[select.getValue()];
+        Intent extras = getIntent();
+        String dishName = extras.getStringExtra("dishSelected");
+
+    }
+
+    /**
+     * Manager decides to go back.
+     *
+     * @param v view
+     */
+    public void selectExit(View v) {
+        Intent intent = new Intent(EditCaloriesActivity.this, SelectPriceOrCaloriesActivity.class);
+        startActivity(intent);
+    }
+
 }
--- Android/app/src/main/java/constant/manager_system/ManagerDecision.java
+++ Android/app/src/main/java/constant/manager_system/ManagerDecision.java
@@ -1,9 +1,12 @@
 package constant.manager_system;
 
 
+import entity.customer.Customer;
+
 /**
  * This class holds all the Manager decision messages in the program
  */
 
-public enum ManagerDecision {DELETE, THREE, TWO, ONE, CALORIES, PRICE, EDIT
+public enum ManagerDecision {DELETE, THREE, TWO, ONE, CALORIES, PRICE, EDIT, FOUR, FIVE, SIX
+
 }
