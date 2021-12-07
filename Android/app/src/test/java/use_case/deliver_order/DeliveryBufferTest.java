9392442d6ecefbfef44c181f47b94fcd0ac8ac74
ddx <xiededong001211@gmail.com>
2021-12-07 18:10:27 -0500

    Update test javadoc

--- Android/app/src/test/java/use_case/deliver_order/DeliveryBufferTest.java
+++ Android/app/src/test/java/use_case/deliver_order/DeliveryBufferTest.java
@@ -13,23 +13,36 @@ import java.util.HashMap;
 public class DeliveryBufferTest {
     private Order testOrder;
 
+    /**
+     * Set up the test
+     */
     @Before
     public void setUp() {
         DeliveryBuffer.setDeliveryBuffer(new ArrayDeque<>());
         testOrder = new DeliveryOrder("1", new HashMap<>());
     }
 
+    /**
+     * test to get a delivery order
+     * @throws Exception When there is no order, throw exception
+     */
     @Test
     public void getDeliveryOrder() throws Exception {
         DeliveryBuffer.addDeliveryOrder(testOrder);
         assertEquals(testOrder, DeliveryBuffer.getDeliveryOrder());
     }
 
+    /**
+     * Test to add a delivery order to the buffer
+     */
     @Test
     public void addDeliveryOrder() {
         DeliveryBuffer.addDeliveryOrder(testOrder);
     }
 
+    /**
+     * Test to reset the buffer
+     */
     @Test
     public void testReset() {
         DeliveryBuffer.reset();
