da76260b5a7280afe77ab6eb6b96d061491f82e1
dabandaidai <naihe.xiao@mail.utoronto.ca>
2021-12-07 07:53:35 -0500

    Adding messages and basic information for this activity.

--- Android/app/src/main/java/com/example/androidgui/menu_activities/SelectPriceOrCaloriesActivity.java
+++ Android/app/src/main/java/com/example/androidgui/menu_activities/SelectPriceOrCaloriesActivity.java
@@ -1,10 +1,14 @@
 package com.example.androidgui.menu_activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import com.example.androidgui.R;
+import constant.manager_system.ManagerDecision;
+import constant.manager_system.ManagerUIMessage;
 import presenter.menu_system.view_interface.SelectPriceOrCaloriesViewInterface;
 
 /**
@@ -27,5 +31,37 @@ public class SelectPriceOrCaloriesActivity extends AppCompatActivity implements 
         setContentView(R.layout.activity_select_price_or_calories);
         select = findViewById(R.id.selectPriceOrCalories);
         askSelection = findViewById(R.id.askSelection);
+
+        setupMessage();
+        setupOptions();
     }
+
+    /**
+     * Setting up message.
+     */
+    private void setupMessage() {
+        askSelection.setText(ManagerUIMessage.PRICE_CALORIES);
+    }
+
+    /**
+     * Setting up options.
+     */
+    private void setupOptions() {
+        selectOption = new String[]{ManagerDecision.CALORIES.toString(), ManagerDecision.PRICE.toString()};
+        select.setMaxValue(selectOption.length - 1);
+        select.setMinValue(0);
+        select.setDisplayedValues(selectOption);
+    }
+
+
+    /**
+     * Manager decides to go back.
+     *
+     * @param v view
+     */
+    public void selectExit(View v) {
+        Intent intent = new Intent(SelectPriceOrCaloriesActivity.this, SelectEditOrDeleteActivity.class);
+        startActivity(intent);
+    }
+
 }
--- Android/app/src/main/java/constant/manager_system/ManagerUIMessage.java
+++ Android/app/src/main/java/constant/manager_system/ManagerUIMessage.java
@@ -8,4 +8,5 @@ public class ManagerUIMessage {
     public static final String MANAGE_DISH = "What dish do you want to manage?";
     public static final String EDIT_DELETE = "Ao you want to edit or delete this dish?";
     public static final String DELETE_REVIEW = "What is the criteria for deletion?";
+    public static final String PRICE_CALORIES = "Do you want to edit price or calories of this dish?";
 }
